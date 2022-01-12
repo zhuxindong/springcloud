6442cb037eb16f927371e64b3bb0a1351d7f78bb
zhuxindong <501801307@qq.com>
2022-01-12 16:58:13 +0800

    重构

--- cloud-provider-payment8001/src/main/java/com/zxd/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/zxd/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.zxd.springcloud.entities.CommonResult;
 import com.zxd.springcloud.entities.Payment;
 import com.zxd.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
 
